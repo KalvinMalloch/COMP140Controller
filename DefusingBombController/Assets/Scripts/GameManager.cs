63017f697e986609aa412e8ee233b39c5dc09cf2
KalvinMalloch <km230683@falmouth.ac.uk>
2020-02-17 06:35:47 +0000

    Created two modules for the sliding potentiometer.

--- DefusingBombController/Assets/Scripts/GameManager.cs
+++ DefusingBombController/Assets/Scripts/GameManager.cs
@@ -1,24 +1,51 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-    private bool countingDown;
+    public bool countingDown;
+    public static int completedModules;
     public float countdownNumber;
+    public Text countdownText;
 
     private void Start()
     {
-        
+        countdownText = countdownText.GetComponent<Text>();
     }
 
     private void countDown()
     {
-        countdownNumber -= Time.deltaTime;
+        if (countingDown == true)
+        {
+            countdownNumber = Mathf.Round(countdownNumber * 100f) / 100f;
+            countdownText.text = countdownNumber.ToString();
+            countdownNumber -= Time.deltaTime;
+        }
+    }
+
+    private void endGame()
+    {
+        if (countdownNumber < 0)
+        {
+            countdownNumber = 0;
+            countdownText.text = countdownNumber.ToString();
+            countingDown = false;
+            if (completedModules == 6)
+            {
+                print("You win.");
+            }
+            else
+            {
+                print("You blow up.");
+            }
+        }
     }
 
     private void Update()
     {
         countDown();
+        endGame();
     }
 }
--- DefusingBombController/Assets/Scripts/SlidingPotentiometer.cs
+++ DefusingBombController/Assets/Scripts/SlidingPotentiometer.cs
@@ -1,16 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SlidingPotentiometer : MonoBehaviour
 {
+    public int slidingChosenNumber;
+    public bool moduleCompleted;
+    public bool minusModuleCompleted;
+    public static int completedModules;
+    public Slider potenSlider;
+    public Text slidingChosenText;
+    public Text slidingPotenText;
+
     void Start()
     {
-        print("Test");
+        slidingChosenNumber = Random.Range(1, 100);
+        slidingChosenText = slidingChosenText.GetComponent<Text>();
+        slidingPotenText = slidingPotenText.GetComponent<Text>();
+        slidingChosenText.text = "Numeral: " + slidingChosenNumber.ToString();
+    }
+
+    private void calculateSlider()
+    {
+        slidingPotenText.text = potenSlider.value.ToString();
+        if (potenSlider.value == slidingChosenNumber & moduleCompleted == false)
+        {
+            completedModules = completedModules + 1;
+            moduleCompleted = true;
+            minusModuleCompleted = true;
+        }
+        else if (potenSlider.value != slidingChosenNumber & minusModuleCompleted == true)
+        {
+            completedModules = completedModules - 1;
+            minusModuleCompleted = false;
+            moduleCompleted = false;
+        }
     }
 
     void Update()
     {
-        
+        print(completedModules);
+        calculateSlider();
     }
 }
