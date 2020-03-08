7116a8b85d700d31e0eed6306e61e19fc7e381cb
KalvinMalloch <km230683@falmouth.ac.uk>
2020-03-08 22:05:35 +0000

    Completed tickbox module and started circle rotation module.

--- DefusingBombController/Assets/Scripts/KeyPoten.cs
+++ DefusingBombController/Assets/Scripts/KeyPoten.cs
@@ -6,15 +6,44 @@ using UnityEngine.UI;
 public class KeyPoten : MonoBehaviour
 {
     public Toggle[] keyToggles;
+    public Text chosenKeyText;
+    public int chosenPotenNumber;
+    public bool moduleCompleted;
+    public bool minusModuleCompleted;
 
     void Start()
     {
-        
+        chosenPotenNumber = Random.Range(1, 3);
+        chosenKeyText = chosenKeyText.GetComponent<Text>();
     }
 
     private void CalculateKeys()
     {
-
+        chosenKeyText.text = "Key: Key " + chosenPotenNumber;
+        if (chosenPotenNumber == 1 & keyToggles[0].isOn == true & moduleCompleted == false)
+        {
+            GameManager.completedModules = GameManager.completedModules + 1;
+            moduleCompleted = true;
+            minusModuleCompleted = true;
+        }
+        else if (chosenPotenNumber == 2 & keyToggles[1].isOn == true & moduleCompleted == false)
+        {
+            GameManager.completedModules = GameManager.completedModules + 1;
+            moduleCompleted = true;
+            minusModuleCompleted = true;
+        }
+        else if (chosenPotenNumber == 3 & keyToggles[2].isOn == true & moduleCompleted == false)
+        {
+            GameManager.completedModules = GameManager.completedModules + 1;
+            moduleCompleted = true;
+            minusModuleCompleted = true;
+        }
+        else if (minusModuleCompleted == true)
+        {
+            GameManager.completedModules = GameManager.completedModules - 1;
+            minusModuleCompleted = false;
+            moduleCompleted = false;
+        }
     }
 
     void Update()
---
+++ DefusingBombController/Assets/Scripts/RotaryCircle.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotaryCircle : MonoBehaviour
+{
+    public GameObject circleStageOne;
+    public GameObject circleStageTwo;
+    public Transform circleRotation;
+    public bool stageOne;
+    public bool stageTwo;
+    public bool endTurning;
+
+    void Start()
+    {
+        
+    }
+
+    private void CalculateRotaryCircle()
+    {
+        if (stageOne == false)
+        {
+            circleRotation = circleStageOne.transform;
+        }
+        if (stageOne == true)
+        {
+            circleRotation = circleStageTwo.transform;
+            stageTwo = true;
+        }
+    }
+
+    private void ChooseRotaryRotation()
+    {
+        if (Input.GetKey(KeyCode.RightArrow) & endTurning == false)
+        {
+            circleRotation.Rotate(0, 0, -5);
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow) & endTurning == false)
+        {
+            circleRotation.Rotate(0, 0, 5);
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            stageOne = true;
+        }
+        if (Input.GetKeyDown(KeyCode.Space) & stageTwo == true)
+        {
+            endTurning = true;
+        }
+    }
+
+    void Update()
+    {
+        CalculateRotaryCircle();
+        ChooseRotaryRotation();
+    }
+}
